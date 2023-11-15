79bdb6bc6264bcb8458e020f0094333a73e8581d
Jackson Smith <jackson16smith@gmail.com>
2023-11-15 12:07:49 -0500

    Added basic navbar component with some styling.

--- src/app/components/nav/Nav.jsx
+++ src/app/components/nav/Nav.jsx
@@ -1,24 +1,29 @@
 import Link from 'next/link'
+import styles from '@/app/page.module.css'
 
 function Nav() {
   return (
-    <div className='navbar'>
-        <div className='container'>
-            <Link href="/">Home</Link>
-            <ul className='navbar-sections'>
-                <li>
+    <div>
+        <div className={styles.container}>
+            <ul className={styles.logo}>
+                <li className={styles.item}>
+                    <Link href="/#" scroll={true}>Home</Link>
+                </li>
+            </ul>
+            <ul className={styles.sections}>
+                <li className={styles.item}>
                     <Link href="/about" scroll={true}>About Us</Link>
                 </li>
-                <li>
+                <li className={styles.item}>
                     <Link href="/services">Services</Link>
                 </li>
-                <li>
+                <li className={styles.item}>
                     <Link href="/projects">Projects</Link>
                 </li>
-                <li>
+                <li className={styles.item}>
                     <Link href="/testimonials">Testimonials</Link>
                 </li>
-                <li>
+                <li className={styles.item}>
                     <Link href="/contact">Contact</Link>
                 </li>
             </ul>
