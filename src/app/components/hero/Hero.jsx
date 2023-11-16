1aa2c953c223dbbf9569e86e8824624d1e3d9058
Jackson Smith <jacksonsmith@Jacksons-MacBook-Pro.local>
2023-11-15 20:29:32 -0500

    Services section styling added with Hero section styling boilerplate.

--- src/app/components/hero/Hero.jsx
+++ src/app/components/hero/Hero.jsx
@@ -1,8 +1,21 @@
-
+import styles from '@/app/components/hero/hero.module.css'
+import Image from 'next/image'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    
+    <div className={styles.container}>
+      <Image src={"/tophead.jpg"} alt='Hero picture' width={902} height={668} className={styles.image}/>
+      <div className={styles.block}>
+        <h2>About us</h2>
+        <p>
+          Imperial Construction Corp, your destination for precision and excellence in construction. Whether it's a new build, 
+          renovation, or remodeling project, we're committed to turning your vision into reality with skilled craftsmanship and 
+          clear communication. Choose us for your project, and experience the difference of working with a construction company 
+          dedicated to making your dream home a reality.
+        </p>
+      </div>
+    </div>
   )
 }
 
--- src/app/components/services/Services.jsx
+++ src/app/components/services/Services.jsx
@@ -1,8 +1,46 @@
-
+import styles from '@/app/components/services/services.module.css'
+import '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseChimney, faBathtub, faKitchenSet, faHammer, faTrowelBricks, faBuilding, faTree } from '@fortawesome/free-solid-svg-icons'
 
 function Services() {
   return (
-    <div>Services</div>
+    <div className={styles.section}>
+      <h2>Services</h2>
+      <div className={styles.container}>
+        <div className={styles.card_light}>
+            <FontAwesomeIcon icon={faHouseChimney} className={styles.icon_dark}/>
+            <div className={styles.line_dark}></div>
+            <h3>Major Remodelling</h3>
+        </div>
+        <div className={styles.card_dark}>
+            <FontAwesomeIcon icon={faBathtub} className={styles.icon_light}/>
+            <div className={styles.line_light}/>
+            <h3>Bathroom & Kitchens</h3>
+        </div>
+        <div className={styles.card_light}>
+            <FontAwesomeIcon icon={faHammer} className={styles.icon_dark}/>
+            <div className={styles.line_dark}></div>
+            <h3>Interior remodelling</h3>
+        </div>
+        <div className={styles.card_dark}>
+            <FontAwesomeIcon icon={faTrowelBricks} className={styles.icon_light}/>
+            <div className={styles.line_light}></div>
+            <h3>Exterior remodeling</h3>
+        </div>
+        <div className={styles.card_light}>
+            <FontAwesomeIcon icon={faBuilding} className={styles.icon_dark}/>
+            <div className={styles.line_dark}></div>
+            <h3>Commercial Services</h3>
+        </div>
+        <div className={styles.card_dark}>
+            <FontAwesomeIcon icon={faTree} className={styles.icon_light}/>
+            <div className={styles.line_light}></div>
+            <h3>Outdoor remodelling</h3>
+        </div>
+      </div>
+      <button>Learn More</button>
+    </div>
   )
 }
 
