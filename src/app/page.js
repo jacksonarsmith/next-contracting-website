eac5a6d4a2d1c4ec0d910ddee4d523666dc53e65
Jackson Smith <jackson16smith@gmail.com>
2024-01-29 19:39:42 -0500

    Removed viewport metadata.

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,6 @@ import Services from './components/services/Services'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
-export const viewport = 'width=device-width, initial-scale=1'
-
 export default function Home() {
   return (
     <div>
